04cb19c0efd051a5e01cce02de7dc397fbcff89c
Ostrovski <vladimir_ostrovski@web.de>
2022-11-12 07:31:42 +0100

    work on power law

--- asymptoticTest.R
+++ asymptoticTest.R
@@ -70,40 +70,45 @@ standardDeviationPowerLaw<-function(parameter, results){
   x=parameter$x
   x=sort(x)
   if (length(results$estimator)==1){
-    
+    xmin=parameter$xmin
+    alpha=results$estimator
   }
   else{
     xmin=results$estimator[1]
-    beta=results$estimator[2]
+    alpha=results$estimator[2]
+  }
+  
+  if (alpha==2){
+    alpha=alpha+0.0000001
   }
   n=length(x)
   
-  ef<-function(m,lambda,t){
-    (m-1)*t-exp(-lambda*t)/lambda
+  ef<-function(m,alpha,t){
+    (m-1)*t+t^(2-alpha)/(2-alpha)
   }
   
-  def<-function(m,lambda,s,t){
-    ef(m,lambda,t)-ef(m,lambda,s)
+  def<-function(m,alpha,s,t){
+    ef(m,alpha,t)-ef(m,alpha,s)
   }
   
   pef<-function(k){
     if (k==0){
-      return(def(0,lambda,0,x[1]))
+      return(def(0,alpha,xmin,x[1]))
     } 
     
     if (k==n){
-      return(-ef(1,lambda,x[n]))
+      return(-ef(1,alpha,x[n]))
     }
     
-    return(def(k/n,lambda,x[k],x[k+1]))
+    return(def(k/n,alpha,x[k],x[k+1]))
   }
   
   
   r=0
-  for (i in c(0:n)){
+  for (i in c(1:n)){
     p2=pef(i)
     
-    for (j in c(0:n)){
+    for (j in c(1:n)){
       p1=min(i,j)/n-i*j/(n^2)
       p3=pef(j)
       
--- testCode/testCode7 variance power law.R
+++ testCode/testCode7 variance power law.R
@@ -1,22 +1,25 @@
 source("testStatistic.R")
 library(pracma)
+library(poweRlaw)
 source("asymptoticTestBootstrapVariance.R")
 source("asymptoticTest.R")
 
 
 # simulate variance of the test statistic and
-# test the performance of different variance estimators for exponential distribution
+# test the performance of different variance estimators for power law distribution
 
 parameter=list()
-set.seed(28072022)
+set.seed(11112022)
 
 n=100
-lambda=1
-parameter$interval=c(0.1,5)
+alpha=2.5
+xmin=1
+parameter$interval=c(2.001,3)
 parameter$nSimulation=200
+parameter$xmin=xmin
 
 distance<-function(x, param){
-  distanceExponentialDistribution(x,param)
+  distancePowerLaw(x,xmin,param)
 }
 
 m=1000
@@ -25,18 +28,19 @@ vB=rep(0,m)
 vA=rep(0,m)
 
 for (i in c(1:m)){
-  x=rlnorm(n,0,1)
+  x=rplcon(n,xmin,alpha)
   #x=rexp(n,lambda)
   parameter$x=x
-  est=minDistanceEstimator(x,distance,lambda, parameter$interval)
+  est=minDistanceEstimator(x,distance,alpha, parameter$interval)
   res=list()
   res$estimator=est
   #vTS[i]=testStatistic(x,distance,est)
-  vB[i]=bootstrapStandardDeviation(parameter,res)
-  #vA[i]=standardDeviationExponential(parameter,res)
+  #vB[i]=bootstrapStandardDeviation(parameter,res)
+  vA[i]=standardDeviationPowerLaw(parameter,res)
   print(i)
 }
 
 #sd(vTS)
-mean(vB)
-#mean(vA)
+#mean(vB)
+mean(vA)
+
